d16fc3000b0c6b0e24e7a1673f52a07f536cc19e
Ilian Pihlajamaa <ilian.pihlajamaa@gmail.com>
2025-09-15 11:15:00 +0200

    more fixes

--- src/Constraints/constraints.jl
+++ src/Constraints/constraints.jl
@@ -3,7 +3,7 @@ module Constraints
 using ..Core
 using LinearAlgebra, StaticArrays
 
-include("constraints_impl.jl")
+include("constraints.jl")
 
 export DistanceConstraints, apply_shake!, apply_rattle!, velocity_verlet_shake_rattle!, remove_com_motion!, constraint_residuals
 
